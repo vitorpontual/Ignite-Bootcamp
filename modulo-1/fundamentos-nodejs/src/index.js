5f1ebbe1580c4308c71a9ebde74f2254b12980d6
Vitor Pontual <vitorpguedes@me.com>
2021-03-08 20:44:43 -0300

    Type url Params

--- modulo-1/fundamentos-nodejs/src/index.js
+++ modulo-1/fundamentos-nodejs/src/index.js
@@ -3,19 +3,27 @@ const express = require('express');
 const app = express();
 const port = 2828 
 
+app.use(express.json())
+
 app.get('/courses', (request, response) => {
+   const query = request.query
+   console.log(query)
    return response.json([
       "Curso 1", "Curso 2", "Curso 3"
    ]);
 });
 
 app.post('/courses', (request, response) => {
+   const body = request.body
+   console.log(body)
    return response.json([
       "Curso 1", "Curso 2", "Curso 3", "Curso 4"
    ]);
 });
 
 app.put("/courses/:id", (request, response) => {
+   const { id } = request.params
+   console.log(id)
    return response.json([
       "Curso 5", "Curso 2", "Curso 3", "Curso 4"
    ]);
