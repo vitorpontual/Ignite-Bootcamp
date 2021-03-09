dbb0047781e5d72bb22140f15d38fe986237f545
Vitor Pontual <vitorpguedes@me.com>
2021-03-09 19:29:31 -0300

    Initial server

--- modulo-1/fin-api/src/index.js
+++ modulo-1/fin-api/src/index.js
@@ -1,8 +1,38 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+
 
 const app = express();
 const port = 2828
 
-app.listen(port, () => console.log(`Server is running http://localhost:${port}`))
+app.use(express.json())
+
+/*
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statemnet []
+ *
+ */
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+   const { cpf, name } = request.body;
+   const id = uuidv4()
+
+   customers.push({
+      cpf,
+      name,
+      id,
+      statement: []
+   })
+
+   return response.status(201).send()
+
+
+});
+
+app.listen(port, () => console.log(`Server is running http://localhost:${port}`));
 
 
