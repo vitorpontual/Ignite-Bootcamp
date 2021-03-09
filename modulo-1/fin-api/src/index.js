cc806c07968359daf661a8432f67179d27e2ba1b
Vitor Pontual <vitorpguedes@me.com>
2021-03-09 20:28:16 -0300

    Deposit

--- modulo-1/fin-api/src/index.js
+++ modulo-1/fin-api/src/index.js
@@ -44,13 +44,30 @@ app.post('/account', (request, response) => {
 
 });
 
-
 app.get('/statement', verifyCPF, (request, response) => {
 
    const { customer } = request;
 
    return response.json(customer.statement);
 });
+
+app.post('/deposit', verifyCPF, (request, response) => {
+   const { description, amount } = request.body;
+
+   const { customer } = request;
+
+   const statementOperation = {
+      description,
+      amount,
+      created_at: new Date(),
+      type: "credit"
+   }
+
+   customer.statement.push(statementOperation);
+
+   return response.status(201).send();
+})
+
 
 app.listen(port, () => console.log(`Server is running http://localhost:${port}`));
 
