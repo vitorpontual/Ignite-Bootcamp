69539964393f937b244ad4da00eb167055f59de6
Vitor Pontual <vitorpguedes@me.com>
2021-03-15 20:20:35 -0300

    change dir

--- modulo-1/desafio-complementares/todo/src/routes/User.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const UserController = require('../app/controllers/UserController')
-
-router.get('/', UserController.index)
-router.post('/', UserController.post)
-
-module.exports = router
--- modulo-1/desafio-complementares/todo/src/server.js
+++
@@ -1,5 +0,0 @@
-const server = require('./index')
-
-server.listen(5050, () => {
-   return console.log('Server is Running http://localhost:5050')
-})
