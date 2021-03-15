4be100b6ac67128b82dd6d28815fb30ff545343f
Vitor Pontual <vitorpguedes@me.com>
2021-03-15 20:54:13 -0300

    Configure vscode debugger

--- modulo-2/projeto-com-typescript/src/server.ts
+++ modulo-2/projeto-com-typescript/src/server.ts
@@ -2,8 +2,15 @@ import express, { response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.json({message: "alo mundo"})
-})
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+  return res.json({name});
+});
 
 app.listen(5555, () => console.log("Server is running!"))
