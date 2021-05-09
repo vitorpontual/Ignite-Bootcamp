fd98f91f145edaf3bac35b137eef0f6aa6df738c
Vitor Pontual <vitorpguedes@me.com>
2021-05-09 14:01:48 -0300

    Save pdf local and S3

--- chapter-6/ignitecertificate/src/functions/genereteCertificate.ts
+++ chapter-6/ignitecertificate/src/functions/genereteCertificate.ts
@@ -1,3 +1,99 @@
-export const handle = async (event) => {
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import fs from "fs";
+import handlebars from "handlebars"
+import dayjs from "dayjs";
+import { S3 } from "aws-sdk";
 
+import { document } from "../utils/dynamodbClient"
+
+interface ICreateCertificate {
+  id: string,
+  name: string,
+  grade: string
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string
+}
+
+async function compile(data:ITemplate) {
+  const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = fs.readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)(data);
+}
+
+export const handle = async (event: any) => {
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
+
+  await document.put({
+    TableName: "users_certificates",
+    Item: {
+      id,
+      name,
+      grade
+    }
+  }).promise();
+
+  const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+  const medal = fs.readFileSync(medalPath, "base64");
+
+  const data:ITemplate = {
+    date: dayjs().format("DD/MM/YYYY"),
+    grade,
+    name,
+    id,
+    medal
+  };
+
+  const content = await compile(data);
+
+  const browser = await chromium.puppeteer.launch({
+    headless: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath
+  });
+  
+  const page = await browser.newPage();
+  
+  await page.setContent(content);
+  
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+    printBackground: true,
+    preferCSSPageSize: true,
+  });
+
+  await browser.close();
+
+  const s3 = new S3();
+
+  await s3.putObject({
+    Bucket: "serverlesscertificationignite",
+    Key: `${id}.pdf`,
+    ACL: "public-read",
+    Body: pdf,
+    ContentType: "application/pdf"
+  })
+  .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: "Certificate created!",
+      url: `https://serverlesscertificationignite.s3-sa-east-1.amazonaws.com/${id}.pdf`
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
 }
