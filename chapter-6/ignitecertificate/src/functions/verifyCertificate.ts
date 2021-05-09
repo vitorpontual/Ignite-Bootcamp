f11a00bc3d28453d37bb5b39b1cb7401dcc41d22
Vitor Pontual <vitorpguedes@me.com>
2021-05-09 14:47:03 -0300

    Verify Function

---
+++ chapter-6/ignitecertificate/src/functions/verifyCertificate.ts
@@ -0,0 +1,46 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+
+import { document } from "@utils/dynamodbClient"
+
+
+export const handle: APIGatewayProxyHandler = async (event) => {
+  const { id } = event.pathParameters;
+
+
+  const response = await document.query({
+    TableName: "users_certificates",
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id": id
+    }
+  }).promise();
+  console.log(response)
+
+  const userCertificate = response.Items[0]
+
+  if(userCertificate){
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Certificate valid",
+        name: userCertificate.name,
+        url: `https://serverlesscertificationignite.s3-sa-east-1.amazonaws.com/${id}.pdf//`
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  }
+
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: "Invalid Certificate"
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+
+
+}
