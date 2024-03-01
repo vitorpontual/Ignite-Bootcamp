2791e94fe191b0816e06b58a20d8b501fbc9c2d2
VITOR PONTUAL <vitorpguedes@me.com>
2024-03-01 10:48:23 -0300

    stream json body

--- nodejs/chapter-1/01-fundamentos-nodejs/src/server.js
+++ nodejs/chapter-1/01-fundamentos-nodejs/src/server.js
@@ -30,8 +30,22 @@ import http from 'node:http'
 // HTTP Status Code - MDN
 const users = []
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async(request, response) => {
   const { method, url } = request
+
+  const buffers = []
+
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  }catch{
+    request.body = null
+  }
+
+  console.log(request.body)
 
   if (method === 'GET' && url === '/users'){
     return response
@@ -39,10 +53,13 @@ const server = http.createServer((request, response) => {
       .end(JSON.stringify(users)) }
 
   if (method === 'POST' && url === '/users'){
+
+    const { name, email} = request.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com'
+      name,
+      email
     })
     return response.writeHead(201).end()
   }
--- nodejs/chapter-1/01-fundamentos-nodejs/streams/stream-http-server.js
+++ nodejs/chapter-1/01-fundamentos-nodejs/streams/stream-http-server.js
@@ -14,11 +14,22 @@ class InverseNumberStream extends Transform {
 
 // req = ReadableStream
 // res = WritableStream
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  const buffers = []
 
-  return req
-    .pipe(new InverseNumberStream())
-    .pipe(res)
+  for await (const chunk of req){
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(fullStreamContent)
+
+  return res.end(fullStreamContent)
+
+  // return req
+  //   .pipe(new InverseNumberStream())
+  //   .pipe(res)
 
 })
 
