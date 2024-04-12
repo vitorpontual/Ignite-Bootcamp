c46c71f8bfe9676422daad4fd0bf486219220824
VITOR PONTUAL <vitorpguedes@me.com>
2024-04-12 10:02:37 -0300

    aulas de Buffer e Middleware

--- nodejs/chapter-1/01-fundamentos-nodejs/src/server.js
+++ nodejs/chapter-1/01-fundamentos-nodejs/src/server.js
@@ -1,55 +1,18 @@
 import http from 'node:http'
+import { json } from './middlaware/json.js'
 
-// CommonJS => require
-// ESModulejs ==> export
-// Criar usuários
-// Listagem de usuários
-// Edição de usuários
-// Remoção de usuários
 
-// HTTP
-// - Método http
-// - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar recurso back-end
-// POST => Criar uma recurso
-// PUT => Editar/ Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação única ou especifica de um recurso no back-end
-// DELETE => Deletar um recurso do backend
-
-// GET /users => Buscando usuários do back-end
-// POST /users => Criar um usuário no back-end
-
-// Stateful - Stateless (revisar)
-// JSON - JavaScript Object Notation
-
-// HEADERS (Requisição/resposta) ==> Metadados
-
-// HTTP Status Code - MDN
 const users = []
 
 const server = http.createServer(async(request, response) => {
   const { method, url } = request
 
-  const buffers = []
-
-  for await (const chunk of request) {
-    buffers.push(chunk)
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString())
-  }catch{
-    request.body = null
-  }
+  await json(request, response)
 
   console.log(request.body)
 
   if (method === 'GET' && url === '/users'){
     return response
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users)) }
 
   if (method === 'POST' && url === '/users'){
@@ -68,4 +31,6 @@ const server = http.createServer(async(request, response) => {
   return response.writeHead(404).end()
 })
 
-server.listen(3333)
+server.listen(3333, () => {
+  console.log("Server Online")
+})
